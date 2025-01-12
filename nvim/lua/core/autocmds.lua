a29bfbecff56f45445736c9abac560b95014debd
Zach Lambert <zachalambert@gmail.com>
2025-01-12 14:19:07 +0000

    Copied current neovim config

---
+++ nvim/lua/core/autocmds.lua
@@ -0,0 +1,78 @@
+-----------------------------------------------------------
+-- Autocommand functions
+-----------------------------------------------------------
+
+-- Define autocommands with Lua APIs
+-- See: h:api-autocmd, h:augroup
+
+local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
+
+-- General settings:
+--------------------
+
+-- Highlight on yank
+augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '1000' })
+  end
+})
+
+-- Remove whitespace on save
+autocmd('BufWritePre', {
+  pattern = '',
+  command = ":%s/\\s\\+$//e"
+})
+
+-- Don't auto commenting new lines
+autocmd('BufEnter', {
+  pattern = '',
+  command = 'set fo-=c fo-=r fo-=o'
+})
+
+-- Settings for filetypes:
+--------------------------
+
+-- Disable line length marker
+augroup('setLineLength', { clear = true })
+autocmd('Filetype', {
+  group = 'setLineLength',
+  pattern = { 'text', 'markdown', 'html', 'xhtml', 'javascript', 'typescript' },
+  command = 'setlocal cc=0'
+})
+
+-- Set indentation to 4 spaces
+augroup('setIndent', { clear = true })
+autocmd('Filetype', {
+  group = 'setIndent',
+  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
+    'yaml', 'lua', 'json', 'bash'
+  },
+  command = 'setlocal shiftwidth=4 tabstop=4'
+})
+
+-- Terminal settings:
+---------------------
+
+-- Open a Terminal on the right tab
+autocmd('CmdlineEnter', {
+  command = 'command! Term :botright vsplit term://$SHELL'
+})
+
+-- Enter insert mode when switching to terminal
+autocmd('TermOpen', {
+  command = 'setlocal listchars= nonumber norelativenumber nocursorline',
+})
+
+autocmd('TermOpen', {
+  pattern = '',
+  command = 'startinsert'
+})
+
+-- Close terminal buffer on process exit
+autocmd('BufLeave', {
+  pattern = 'term://*',
+  command = 'stopinsert'
+})
