d88878719f919451bea1b1a7437341ecfef828e8
Zach Lambert <zach@kudan.eu>
2025-12-16 17:14:57 +0000

    Fixing new features

--- features/src/zach-dotfiles/install.sh
+++ features/src/zach-dotfiles/install.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-git clone https://github.com/zachlambert/dev-tools.git
-cd dev-tools/dotfiles
+sudo touch /is-this-created.txt
+cd /home/kudan
+touch test-dotfiles.txt
+exit 0
 
-cp -r nvim ~/.config/.config/nvim
-cp -r shell/shell ~/.config/.config/shell
-cp -r shell/bash ~/.config/.config/bash
-cp -r shell/zsh ~/.config/.config/zsh
-cp -r shell/.bashrc ~/.config/.bashrc
-cp -r shell/.zshrc ~/.config/.zshrc
-cp git/gitignore ~/.config/gitignore
-
-cd ../..
-rm -r dev-tools
-
-# First run of nvim -> install plugins
-nvim --headless -V +qall
-
-# Set git settings
-git config --global core.excludesfile ~/.config/gitignore
-
-# Install extra useful packages
-apt update && \
-  apt install -y less fzf xclip && \
-  rm -rf /var/lib/apt/lists/*
-
+# mkdir -p /home/$USER/.config
+# 
+# git clone https://github.com/zachlambert/dev-tools.git
+# cd dev-tools/dotfiles
+# 
+# mkdir -p /home/$USER/.config
+# cp -r nvim /home/$USER/.config/nvim
+# cp -r shell/shell /home/$USER/.config/shell
+# cp -r shell/bash /home/$USER/.config/bash
+# cp -r shell/zsh /home/$USER/.config/zsh
+# cp -r shell/.bashrc /home/$USER/.bashrc
+# cp -r shell/.zshrc /home/$USER/.zshrc
+# cp git/gitignore /home/$USER/.config/gitignore
+# 
+# cd ../..
+# rm -r dev-tools
+# 
+# # First run of nvim -> install plugins
+# nvim --headless -V +qall
+# 
+# # Set git settings
+# git config --global core.excludesfile /home/$USER/.config/gitignore
+# 
+# # Install extra useful packages
+# apt update && \
+#   apt install -y less fzf xclip && \
+#   rm -rf /var/lib/apt/lists/*
+# 
