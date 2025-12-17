b0b4bf214bd9094cf32f7ad444f8774b5cecb6ed
Zach Lambert <zach@kudan.eu>
2025-12-17 10:56:25 +0000

    Debugging features

--- features/src/zsh/install.sh
+++ features/src/zsh/install.sh
@@ -3,8 +3,18 @@
 apt update
 apt install -y zsh
 
+cd /home/$_CONTAINER_USER
+if [ -f .zshrc ]; then
+  mv .zshrc .zshrc-prior
+fi
+
 su $_CONTAINER_USER -c 'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
 su $_CONTAINER_USER -c 'git clone "https://github.com/zsh-users/zsh-autosuggestions" "/home/$USER/.oh-my-zsh/plugins/zsh-autosuggestions"'
 su $_CONTAINER_USER -c 'git clone "https://github.com/zsh-users/zsh-syntax-highlighting" "/home/$USER/.oh-my-zsh/plugins/zsh-syntax-highlighting"'
+
+rm .zshrc
+if [ -f .zshrc-prior ]; then
+  mv .zshrc-prior .zshrc
+fi
 
 rm -rf /var/lib/apt/lists/*
