178900345888ac762a5714bd845d8599278855d1
Zach Lambert <zach@kudan.eu>
2025-12-17 10:23:15 +0000

    Debugging features

--- features/src/zach-dotfiles/install.sh
+++ features/src/zach-dotfiles/install.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-cd /home/kudan
-
-touch feature-debug.txt
-echo "_CONTAINER_USER=$_CONTAINER_USER" >> feature-debug.txt
-echo "_REMOTE_USER=$_REMOTE_USER" >> feature-debug.txt
-echo "USER=$USER" >> feature-debug.txt
-# echo "_CONTAINER_USER=${_CONTAINER_USER}" >> feature-debug.txt
-# echo "_REMOTE_USER=${_REMOTE_USER}" >> feature-debug.txt
-# echo "USER=${USER}" >> feature-debug.txt
-echo "Whomai=$(whoami)" >> feature-debug.txt
-
-sudo su $_CONTAINER_USER
+cd /home/$_CONTAINER_USER
 
 mkdir -p .config
 
@@ -26,13 +15,14 @@ cp -r $dotfiles/shell/.bashrc .bashrc
 cp -r $dotfiles/shell/.zshrc .zshrc
 cp $dotfiles/git/gitignore .config/gitignore
 
-# rm -r dev-tools
+chown -R $_CONTAINER_USER:$_CONTAINER_USER .config
+rm -r dev-tools
 
 # First run of nvim -> install plugins
-nvim --headless -V +qall
+su $_CONTAINER_USER -c nvim --headless -V +qall
 
 # Set git settings
-git config --global core.excludesfile /home/$USER/.config/gitignore
+su $_CONTAINER_USER git config --global core.excludesfile /home/$_CONTAINER_USER/.config/gitignore
 
 # Install extra useful packages
 # apt update && \
