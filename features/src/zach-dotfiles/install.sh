6c4aaf0119622eeaaaed6fc9fec7e3fb5b265943
Zach Lambert <zach@kudan.eu>
2025-12-17 09:49:01 +0000

    Debugging features

--- features/src/zach-dotfiles/install.sh
+++ features/src/zach-dotfiles/install.sh
@@ -1,34 +1,31 @@
 #!/bin/sh
 
-sudo touch /is-this-created.txt
 cd /home/kudan
-touch test-dotfiles.txt
+echo "User=$USER" >> what-is-user.txt
 
-mkdir -p /home/$USER/.config
-# 
-# git clone https://github.com/zachlambert/dev-tools.git
-# cd dev-tools/dotfiles
-# 
-# mkdir -p /home/$USER/.config
-# cp -r nvim /home/$USER/.config/nvim
-# cp -r shell/shell /home/$USER/.config/shell
-# cp -r shell/bash /home/$USER/.config/bash
-# cp -r shell/zsh /home/$USER/.config/zsh
-# cp -r shell/.bashrc /home/$USER/.bashrc
-# cp -r shell/.zshrc /home/$USER/.zshrc
-# cp git/gitignore /home/$USER/.config/gitignore
-# 
-# cd ../..
+mkdir -p .config
+
+git clone https://github.com/zachlambert/dev-tools.git
+cd dev-tools/dotfiles
+
+mkdir -p .config
+cp -r nvim .config/nvim
+cp -r shell/shell .config/shell
+cp -r shell/bash .config/bash
+cp -r shell/zsh .config/zsh
+cp -r shell/.bashrc .bashrc
+cp -r shell/.zshrc .zshrc
+cp git/gitignore .config/gitignore
+
 # rm -r dev-tools
-# 
-# # First run of nvim -> install plugins
-# nvim --headless -V +qall
-# 
-# # Set git settings
-# git config --global core.excludesfile /home/$USER/.config/gitignore
-# 
-# # Install extra useful packages
-# apt update && \
-#   apt install -y less fzf xclip && \
-#   rm -rf /var/lib/apt/lists/*
-# 
+
+# First run of nvim -> install plugins
+nvim --headless -V +qall
+
+# Set git settings
+git config --global core.excludesfile /home/$USER/.config/gitignore
+
+# Install extra useful packages
+apt update && \
+  apt install -y less fzf xclip && \
+  rm -rf /var/lib/apt/lists/*
