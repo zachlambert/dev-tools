79c5ce889430c1320535c2cb8f8edf0e19025057
Zach Lambert <zach@kudan.eu>
2026-01-20 14:40:03 +0000

    Added script for running devcontainer with required args

---
+++ install-devcli.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+install_path=/usr/bin/devcli
+
+if [[ -f $install_path ]]; then
+  sudo rm /usr/bin/devcli
+fi
+
+if [[ $# -gt 0 ]] && [[ $1 == "-s" ]]; then
+  echo "Symlink install: $install_path"
+  sudo ln -s $(pwd)/devcli $install_path
+else
+  echo "Install: $install_path"
+  sudo cp devcli install_path
+fi
