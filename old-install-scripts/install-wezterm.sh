b8738489c6be659b4d272b8dd6b6a17bd7feed53
Zach Lambert <zach@kudan.eu>
2026-01-20 14:41:50 +0000

    Removed unused scripts and Dockerfile

--- old-install-scripts/install-wezterm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt remove wezterm
-sudo apt install wezterm-nightly
-
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which wezterm) 20
-sudo update-alternatives --set x-terminal-emulator $(which wezterm)
--- test-features.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-devcontainer features test features
